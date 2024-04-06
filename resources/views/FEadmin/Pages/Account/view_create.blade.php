b1709a7c67b3e74ab3dc2b938a7162fed67e4de1
TienDat <garena.hp02@gmail.com>
2024-04-06 08:53:52 +0700

    sửa code

--- resources/views/FEadmin/Pages/Account/view_create.blade.php
+++ resources/views/FEadmin/Pages/Account/view_create.blade.php
@@ -28,7 +28,7 @@
         <!-- [ breadcrumb ] end -->
         <!-- [ Main Content ] start -->
         <div class="row">
-            <div class="col-sm-12 col-md-6 offset-md-3 col-lg-6 offset-lg-3">
+            <div class="col-sm-12 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2 col-xxl-6 offset-xxl-3">
                 <!-- Basic Inputs -->
                 <form class="card" action="{{ route('creater_account') }}" method="POST" id="formReset">
                     @csrf
