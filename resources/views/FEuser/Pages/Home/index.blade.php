84d828da63c862958a1fef6f88a193cddad49723
DESKTOP-QD7JEVF\ADMIN <garena.hp02@gmail.com>
2024-04-12 17:39:50 +0700

    sdasdasda

--- resources/views/FEuser/Pages/Home/index.blade.php
+++ resources/views/FEuser/Pages/Home/index.blade.php
@@ -35,15 +35,13 @@
                                     </div><!--end row-->
                                 </div>
                                 <!--end row-->
+                            </form>
                         </div>
-                        </form>
                         <!--end form-->
                     </div>
                 </div>
             </div>
             <!--end col-->
-        </div>
-        <!--end row-->
         </div>
         <!--end container-->
 
@@ -85,151 +83,57 @@
                             $address .= $value->wards;
                         }
                     @endphp
-                    <div class="col-12">
+                    <div class="col-xl-12 col-lg-4 col-md-6 col-sm-6 col-12">
                         <div
-                            class="job-post job-post-list rounded shadow p-4 d-md-flex align-items-center justify-content-between position-relative">
-                            <div class="d-flex align-items-center w-400px">
+                            class="job-post job-post-list rounded shadow p-3 d-xl-flex align-items-center justify-content-between position-relative">
+                            <div class="d-flex align-items-center w-350px">
                                 <div>
-                                    <a href="{{ $value->linkGroup }}" target="_blank"
-                                        class="h5 title text-dark">{{ $value->nameGroup }}</a>
-                                    <span class="d-flex fw-medium mt-md-2">{{ $value->account_group }} Thành Viên</span>
+                                    <div class="truncate-mobile">
+                                        <a href="{{ $value->linkGroup }}" target="_blank"
+                                            class="h5 title text-dark">{{ $value->nameGroup }}</a>
+
+                                    </div>
+                                    <span class="d-flex fw-medium mt-md-2">{{ $value->account_group }} Thành
+                                        Viên</span>
                                 </div>
                             </div>
 
-                            <div class="d-flex align-items-center justify-content-between d-md-block mt-3 mt-md-0 w-200px">
+                            <div class="d-flex align-items-center justify-content-between d-xl-block mt-3 mt-md-0 w-120px">
                                 <span class="badge rounded-pill {{ $typeColor }}">{{ $typeName }}</span>
-                                <span class="text-muted d-flex align-items-center fw-medium mt-md-2"><i
+                                <span class="text-muted d-flex align-items-center fw-medium mt-lg-2"><i
                                         class="fea icon-sm me-1 align-middle fas fa-map-signs"></i>{{ $value->category }}</span>
                             </div>
 
-                            <div class="d-blog align-items-center justify-content-between d-md-block mt-3 mt-md-0 w-250px">
+                            <div class="d-blog align-items-center justify-content-between d-xl-block mt-3 mt-md-0 w-280px">
                                 <span class="text-muted d-flex align-items-center fw-medium mt-md-2">
-                                    Giá Thuê: {{ number_format($value->rent_cost, 0, ',', '.') }} vnđ / tháng</span>
+                                    Giá Thuê: {{ number_format($value->rent_cost, 0, ',', '.') }} vnđ /
+                                    tháng</span>
                                 <span class="text-muted d-flex align-items-center fw-medium mt-md-2 mb-10px">
                                     Giá Bán: {{ number_format($value->price, 0, ',', '.') }} vnđ</span>
                             </div>
 
-                            @if (!empty($address))
-                                <div
-                                    class="d-blog align-items-center justify-content-between d-md-block mt-2 mt-md-0 w-350px">
-                                    <span class="text-muted d-flex align-items-center"><i data-feather="map-pin"
-                                            class="fea icon-sm me-1 align-middle"></i>{{ $value->objCategory->name }}</span>
-                                    <span class="d-flex fw-medium mt-md-2 mb-10px"
-                                        style="font-size: 11.5px;">{{ $address }}</span>
-                                </div>
-                            @else
-                                <div
-                                    class="d-blog align-items-center justify-content-between d-md-block mt-2 mt-md-0 w-350px">
-                                    <span class="text-muted d-flex align-items-center"><i data-feather="map-pin"
-                                            class="fea icon-sm me-1 align-middle"></i>Vị Trí</span>
-                                    <span class="d-flex fw-medium mt-md-2 mb-10px"
-                                        style="font-size: 11.5px;">{{ $value->objCategory->name }}</span>
-                                </div>
-                            @endif
+                            <div
+                                class="d-blog align-items-center justify-content-between d-xl-block mt-2 mt-md-0 w-350px truncate">
+                                <span class="text-muted d-flex align-items-center mt-md-2"><i data-feather="map-pin"
+                                        class="fea icon-sm me-1 align-middle"></i>{{ $value->objCategory->name }}</span>
+                                <span class="d-flex fw-medium mt-md-2 mb-10px mb-md-2"
+                                    style="font-size: 11.5px;">{{ $address }}</span>
+                            </div>
 
                             <div class="mt-3 mt-md-0">
                                 <a href="{{ route('view_detail_group', $value->slugGroup) }}"
-                                    class="btn btn-sm btn-primary w-full ms-md-1">Đăng Ký</a>
+                                    class="btn btn-sm btn-primary w-full">Đăng Ký</a>
                             </div>
                         </div>
-                    </div>
-                    <div class="row g-4">
-                        @foreach ($list_interact as $key => $value)
-                            @php
-                                $type = $typeHTML[$value->type] ?? [
-                                    'name' => 'Không Xác Định',
-                                    'color' => '#000000',
-                                ];
-                                $typeName = $type['name'];
-                                $typeColor = $type['color'];
-                                $address = '';
-
-                                // Kiểm tra và thêm thông tin thành phố
-                                if (isset($value->province)) {
-                                    $address .= $value->province;
-                                }
-
-                                // Kiểm tra và thêm thông tin quận huyện
-                                if (isset($value->district)) {
-                                    if ($address !== '') {
-                                        $address .= ' - ';
-                                    }
-                                    $address .= $value->district;
-                                }
-
-                                // Kiểm tra và thêm thông tin phường xã
-                                if (isset($value->wards)) {
-                                    if ($address !== '') {
-                                        $address .= ' - ';
-                                    }
-                                    $address .= $value->wards;
-                                }
-                            @endphp
-                            <div class="col-xl-12 col-lg-4 col-md-6 col-sm-6 col-12">
-                                <div
-                                    class="job-post job-post-list rounded shadow p-3 d-xl-flex align-items-center justify-content-between position-relative">
-                                    <div class="d-flex align-items-center w-350px">
-                                        <div>
-                                            <div class="truncate-mobile">
-                                                <a href="{{ $value->linkGroup }}" target="_blank"
-                                                    class="h5 title text-dark">{{ $value->nameGroup }}</a>
-
-                                            </div>
-                                            <span class="d-flex fw-medium mt-md-2">{{ $value->account_group }} Thành
-                                                Viên</span>
-                                        </div>
-                                    </div>
-
-                                    <div
-                                        class="d-flex align-items-center justify-content-between d-xl-block mt-3 mt-md-0 w-120px">
-                                        <span class="badge rounded-pill {{ $typeColor }}">{{ $typeName }}</span>
-                                        <span class="text-muted d-flex align-items-center fw-medium mt-lg-2"><i
-                                                class="fea icon-sm me-1 align-middle fas fa-map-signs"></i>{{ $value->category }}</span>
-                                    </div>
-
-                                    <div
-                                        class="d-blog align-items-center justify-content-between d-xl-block mt-3 mt-md-0 w-280px">
-                                        <span class="text-muted d-flex align-items-center fw-medium mt-md-2">
-                                            Giá Thuê: {{ number_format($value->rent_cost, 0, ',', '.') }} vnđ /
-                                            tháng</span>
-                                        <span class="text-muted d-flex align-items-center fw-medium mt-md-2 mb-10px">
-                                            Giá Bán: {{ number_format($value->price, 0, ',', '.') }} vnđ</span>
-                                    </div>
-
-                                    <div
-                                        class="d-blog align-items-center justify-content-between d-xl-block mt-2 mt-md-0 w-350px truncate">
-                                        <span class="text-muted d-flex align-items-center mt-md-2"><i data-feather="map-pin"
-                                                class="fea icon-sm me-1 align-middle"></i>{{ $value->objCategory->name }}</span>
-                                        <span class="d-flex fw-medium mt-md-2 mb-10px mb-md-2"
-                                            style="font-size: 11.5px;">{{ $address }}</span>
-                                    </div>
-
-                                    <div class="mt-3 mt-md-0">
-                                        <a href="{{ route('view_detail_group', $value->slugGroup) }}"
-                                            class="btn btn-sm btn-primary w-full">Đăng Ký</a>
-                                    </div>
-                                </div>
-                            </div><!--end col-->
-                        @endforeach
-                    </div><!--end row-->
-                    <div class="row" style="margin: 0px 0px 50px 0px;">
-                        <div class="col-12 mt-4 pt-2" style="text-align: center;">
-                            <a href="{{ route('view_group_interact') }}" class="btn btn-sm btn-primary w-full">Xem Thêm</a>
-                        </div><!--end col-->
-                    </div><!--end row-->
-            </div><!--end container-->
-
-            <div class="container">
-                <div class="alert alert-success" style="padding: 0px; background: #341eb9; border-color: #341eb9;">
-                    <img loading="lazy" class="responsite-image-text"
-                        src="{{ url('assets') }}/images/Minimalist_Blue_Green_Real_Estate_Company_Group_Logo__2_-removebg-preview.png"
-                        style="margin-left: 15px;" alt="grid">
-                </div>
-                <!--end col-->
-            </div>
-            <!--end row-->
-        </div>
-        <!--end container-->
+                    </div><!--end col-->
+                @endforeach
+            </div><!--end row-->
+            <div class="row" style="margin: 0px 0px 50px 0px;">
+                <div class="col-12 mt-4 pt-2" style="text-align: center;">
+                    <a href="{{ route('view_group_interact') }}" class="btn btn-sm btn-primary w-full">Xem Thêm</a>
+                </div><!--end col-->
+            </div><!--end row-->
+        </div><!--end container-->
 
         <div class="container">
             <div class="alert alert-success" style="padding: 0px; background: #341eb9; border-color: #341eb9;">
